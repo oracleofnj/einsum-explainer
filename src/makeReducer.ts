ead5f6a34108b525126550b9d5d322f483b92d94
oracleofnj <jared.samet@aya.yale.edu>
2019-04-24 16:03:07 -0400

    Remove unnecessary check

--- src/makeReducer.ts
+++ src/makeReducer.ts
@@ -15,25 +15,13 @@ export type Duck = {
 };
 
 function makeReducer(ducks: Duck[]): Reducer {
-  const appReducers: { [key: string]: Reducer } = ducks.reduce(
-    (obj: { [key: string]: Reducer }, duck) => {
-      obj[duck.type] = duck.reducer;
-      return obj;
-    },
-    {}
-  );
+  const appReducers: { [key: string]: Reducer } = {};
+  ducks.forEach(duck => {
+    appReducers[duck.type] = duck.reducer;
+  });
 
-  function reducer(state: AppState, action: AppAction): AppState {
-    const actionReducer = appReducers[action.type];
-
-    if (actionReducer) {
-      return actionReducer(state, action);
-    } else {
-      throw new TypeError(JSON.stringify(action));
-    }
-  }
-
-  return reducer;
+  return (state: AppState, action: AppAction): AppState =>
+    appReducers[action.type](state, action);
 }
 
 export { makeReducer };
