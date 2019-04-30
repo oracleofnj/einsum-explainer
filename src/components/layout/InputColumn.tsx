8a305bc054bea2ceb06f2508078068b9acd4a272
oracleofnj <jared.samet@aya.yale.edu>
2019-04-29 23:45:06 -0400

    Mobile tweaks

--- src/components/layout/InputColumn.tsx
+++ src/components/layout/InputColumn.tsx
@@ -9,7 +9,7 @@ export default function InputColumn({ prompt, control }: InputColumnProps) {
   return (
     <div className="row">
       <div className="col-xs-4 col-lg-3">{prompt}</div>
-      <div className="col-xs-8 col-lg-9">{control}</div>
+      <div className="col-xs-7 col-lg-9">{control}</div>
     </div>
   );
 }
