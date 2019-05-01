7cfe2d20fbad61fbf5f20a3cbbb79f6725d54a25
oracleofnj <jared.samet@aya.yale.edu>
2019-05-01 14:59:30 -0400

    More presets

--- src/appstate/presets.ts
+++ src/appstate/presets.ts
@@ -11,72 +11,94 @@ type Preset = {
   presetFn: () => PartialAppState;
 };
 
-function matrixMultiplication(): PartialAppState {
-  return {
-    equation: "ij,jk->ik",
-    operandStates: [makeOperand([2, 3], 0, 1), makeOperand([3, 5], 0, 10)]
-  };
-}
-
-function matrixTransposition(): PartialAppState {
-  return {
-    equation: "ji",
-    operandStates: [makeOperand([2, 3], 0, 1)]
-  };
-}
-
-function sum2D(): PartialAppState {
-  return {
-    equation: "ij->",
-    operandStates: [makeOperand([2, 3], 0, 1)]
-  };
-}
-
-function columnSum(): PartialAppState {
-  return {
-    equation: "ij->j",
-    operandStates: [makeOperand([2, 3], 0, 1)]
-  };
-}
-
-function rowSum(): PartialAppState {
-  return {
-    equation: "ij->i",
-    operandStates: [makeOperand([2, 3], 0, 1)]
-  };
-}
-
-function getDiagonal(): PartialAppState {
-  return {
-    equation: "ii->i",
-    operandStates: [makeOperand([3, 3], 0, 1)]
-  };
-}
-
 const presets: { [key: string]: Preset } = {
   matrixMultiplication: {
     description: "Matrix Multiplication",
-    presetFn: matrixMultiplication
+    presetFn: () => ({
+      equation: "ij,jk->ik",
+      operandStates: [makeOperand([2, 3], 0, 1), makeOperand([3, 5], 0, 10)]
+    })
   },
   matrixTransposition: {
     description: "Matrix Transposition (Implicit)",
-    presetFn: matrixTransposition
+    presetFn: () => ({
+      equation: "ji",
+      operandStates: [makeOperand([2, 3], 0, 1)]
+    })
   },
   sum2D: {
     description: "Sum All Elements (2D)",
-    presetFn: sum2D
+    presetFn: () => ({
+      equation: "ij->",
+      operandStates: [makeOperand([2, 3], 0, 1)]
+    })
   },
   columnSum: {
     description: "Sum Each Column",
-    presetFn: columnSum
+    presetFn: () => ({
+      equation: "ij->j",
+      operandStates: [makeOperand([2, 3], 0, 1)]
+    })
   },
   rowSum: {
     description: "Sum Each Row",
-    presetFn: rowSum
+    presetFn: () => ({
+      equation: "ij->i",
+      operandStates: [makeOperand([2, 3], 0, 1)]
+    })
   },
   getDiagonal: {
     description: "Take the Diagonal",
-    presetFn: getDiagonal
+    presetFn: () => ({
+      equation: "ii->i",
+      operandStates: [makeOperand([3, 3], 0, 1)]
+    })
+  },
+  matrixVectorMultiplication: {
+    description: "Matrix-Vector Multiplication",
+    presetFn: () => ({
+      equation: "ij,j->i",
+      operandStates: [makeOperand([2, 3], 0, 1), makeOperand([3], 0, 10)]
+    })
+  },
+  outerProduct: {
+    description: "Outer Product",
+    presetFn: () => ({
+      equation: "i,j->ij",
+      operandStates: [makeOperand([3], 0, 1), makeOperand([4], 10, 10)]
+    })
+  },
+  batchMM1: {
+    description: "Batch Matrix Multiplication (single RHS)",
+    presetFn: () => ({
+      equation: "nij,jk->nik",
+      operandStates: [makeOperand([4, 2, 5], 0, 1), makeOperand([5, 3], 0, 10)]
+    })
+  },
+  batchMM2: {
+    description: "Batch Matrix Multiplication (multiple RHS)",
+    presetFn: () => ({
+      equation: "nij,njk->nik",
+      operandStates: [makeOperand([4, 2, 5], 0, 1), makeOperand([4, 5, 3], 0, 10)]
+    })
+  },
+  tensorContraction: {
+    description: "Tensor Contraction",
+    presetFn: () => ({
+      equation: "pqrs,tuqvr->pstuv",
+      operandStates: [makeOperand([2, 3, 5, 7], 0, 1), makeOperand([11, 13, 3, 17, 5], 0, 10)]
+    })
+  },
+  bilinearTransformation: {
+    description: "Bilinear Transformation",
+    presetFn: () => ({
+      equation: "ik,jkl,il->ij",
+      operandStates: [
+        makeOperand([2, 3], 0, 1),
+        makeOperand([5, 3, 7], 0, 10),
+        makeOperand([2, 7], 0, 100)
+      ]
+    })
   }
 };
 
